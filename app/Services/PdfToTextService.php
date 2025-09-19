13097d7ffc8caae0bd6823a6a456bf9e7a1ddb9d
arisatrio <arysatrio097@gmail.com>
2025-09-19 15:20:27 +0700

    update

--- app/Services/PdfToTextService.php
+++ app/Services/PdfToTextService.php
@@ -8,9 +8,14 @@ class PdfToTextService
 {
     public function convert(string $pdfFilePath): string
     {
-        $text = Pdf::getText($pdfFilePath);
+        // $text = Pdf::getText($pdfFilePath);
+
+        $parser =  new \Smalot\PdfParser\Parser();
+        $pdf = $parser->parseFile($pdfFilePath);
+        $text = $pdf->getText();
         // Ensure valid UTF-8 and filter out malformed characters
         $text = mb_convert_encoding($text, 'UTF-8', 'UTF-8');
+
         return $text;
     }
 }
